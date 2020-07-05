3662b89c6059fe2b50a1a7a6e670110b6805df73
ShyrenMore <shyren.more30@gmail.com>
2020-07-05 10:19:44 +0530

    Update adminviewproduct.php

--- adminviewproduct.php
+++ adminviewproduct.php
@@ -10,20 +10,39 @@
 </head>
 <body>
 <div class="card">
-<div class="image">
-    <img src="productpic.jpg" alt="#" style="width:100%">
+    <div class="image">
+    <img src="product2.jpg" alt="#" style="width:100%">
+    </div>
+    <div class="cost">
+    <h3>30.00</h3>
+    </div>
+    <div class="name">
+    <p>Toothpaste</p>
+    </div>
+    <div class=btn>
+    <form action="delete.php" method="post">
+    <input type="submit"  value="Delete">
+    </form>
+    </div>
 </div>
-<div class="cost">
-<h3>30.00</h3>
+
+<div class="card">
+    <div class="image">
+    <img src="product3.jpg" alt="#" style="width:100%">
+    </div>
+    <div class="cost">
+    <h3>50.00</h3>
+    </div>
+    <div class="name">
+    <p>Soap</p>
+    </div>
+    <div class=btn>
+    <form action="delete.php" method="post">
+    <input type="submit"  value="Delete">
+    </form>
+    </div>
 </div>
-<div class="name">
-<p>Toothpaste</p></div>
-<div class=btn>
-<form action="delete.php" method="post">
-<input type="submit"  value="Delete">
-</form>
-</div>
-</div>
+
 
 </body>
 </html>
