7c88dbefeb42874b777497c44d4d961e2e4c63cc
ShyrenMore <shyren.more30@gmail.com>
2020-07-05 10:04:43 +0530

    Update customerindex.php

--- customerindex.php
+++ customerindex.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"></link>    
+
     <link rel="stylesheet" href="customerindex.css" type="text/css">
     <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
     <title>User Home Page</title>
@@ -14,17 +16,20 @@ if ($_POST["loginsuccess"] == 1)
 {
 
 ?>
-    <div class="container">
-        <div class="tile1">
-            <h1>Buy</h1>
-        </div>
-        <div class="tile2">
-            <h1>View Orders</h1>
-        </div>
-        <div class="tile3">
-            <h1>Cart</h1>
-        </div>
+<div class="row">
+    <div class="col-md-4">
+    <img src="avatar.png" alt="">
     </div>
+    <div class="col-md-4">
+    <div class="v1"></div>
+</div>
+
+<div class="col-md-4"> <button class="form1" ><a href="customerviewproduct.php">View Products</a></button></div>   
+<div class= "col-md-4"><button class="form2">View Orders</button></div>
+<div class="col-md-4"> <button class="form3 mt-3"><a href="cart.php">View Cart</a></button> </div>   
+<!-- </div> -->
+</div>
+
 
 <?php
 }
